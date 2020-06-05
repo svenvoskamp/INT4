b22dd0dba4144219219f5a1e0b36a39f12a0c3c4
svenvoskamp <sven.voskamp@student.howest.be>
2020-06-05 10:39:54 +0200

    Setting up files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function App() {
+
+  return (
+      <>
+        <p>Hallo</p>
+      </>
+  );
+}
+
+export default App;
---
+++ src/consts/index.js
@@ -0,0 +1,4 @@
+const ROUTES = {
+};
+
+export { ROUTES };
