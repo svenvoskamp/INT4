7bbd563613657508b6b86c8e830ac0cfed755569
svenvoskamp <sven.voskamp@student.howest.be>
2020-06-16 19:01:02 +0200

    Little change in type.js

--- src/components/Type/Type.js
+++ src/components/Type/Type.js
@@ -7,9 +7,9 @@ const Type = ({type, setType}) => {
   return (
     <div className={style.option}>
       <label className={style.option_label} htmlFor = {type.id}>
-      <video className={style.option_video} width="400" height = "400"  loop autoPlay muted>
-          <source src={type.img} type="video/webm"/>
-          <source src={type.img} type="video/webm"/>
+      <video id = {type.type} className={style.option_video} width="400" height = "400" autoPlay loop playsInline muted>
+          <source src={type.webm} type="video/webm"/>
+          <source src={type.mp4} type="video/mp4"/>
         </video>
         <input className={style.checkbox_type} id = {type.id} type = "radio" value = {type.id} name = "type" onClick={e=> setType(e.currentTarget.value)} required />
         <p className={style.option_text}>{type.type}</p>
